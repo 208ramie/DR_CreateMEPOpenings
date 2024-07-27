de627d499fcd4b83299d5c0c586651b4b6ef3c25
208ramie <208ramyy98@gmail.com>
2024-07-27 22:50:16 +0300

    Small updates to the view class

--- DiRoots_MEPWallOpenings/Views/CreateMEPOpeningsView.xaml.cs
+++ DiRoots_MEPWallOpenings/Views/CreateMEPOpeningsView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DiRoots_MEPWallOpenings.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace DiRoots_MEPWallOpenings.Views
         private CreateMEPOpeningsView()
         {
             InitializeComponent();
+            this.Closing += new CancelEventHandler(Window_Closing);
         }
 
 
@@ -31,5 +33,29 @@ namespace DiRoots_MEPWallOpenings.Views
         public static CreateMEPOpeningsView Instance
             => _instance ??= new CreateMEPOpeningsView();
 
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            // Set the window's visibility to Hidden
+            this.Visibility = Visibility.Hidden;
+
+            // Cancel the closing event
+            e.Cancel = true;
+        }
+
+
+        private void MergeOpeningsCheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkbox = (CheckBox)sender;
+            if ((bool)checkbox.IsChecked)
+            {
+                MinDistanceTextBox.IsEnabled = true;
+            }
+            else
+            {
+                MinDistanceTextBox.IsEnabled = false;
+            }
+            
+        }
     }
 }
